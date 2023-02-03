26e1dd759b8b5f721631c23fe357a56647eae319
rodrigospicker1 <r.spicker13@gmail.com>
2023-02-03 09:38:21 -0300

    change_image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,33 @@
 import React from "react";
+import memesData from "../memesData.js"
 
 export default function Meme(){
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/4/1g8my4.jpg"
+    })
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    function getMemeImage(){
+        const memesArray = allMemeImages.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
+
     return(
         <main>
-            <form className="form">
+            <div className="form">
                 <input type="text" placeholder="Texto do topo" className="form--input" />
                 <input type="text" placeholder="Texto do fundo" className="form--input" />
-                <button className="form--button">Gerar uma nova imgem</button>
-            </form>
+                <button onClick={getMemeImage} className="form--button">Gerar uma nova imgem</button>
+            </div>
+            <img src={meme.randomImage} title={meme.randomImage} className="meme--image" />
         </main>
     )
 }
---
+++ src/memesData.js
@@ -0,0 +1,47 @@
+export default {
+    "success": true,
+    "data": {
+        "memes": [
+            {
+                "id": "1",
+                "url": "https://i.imgflip.com/4/30b1gx.jpg"
+            },
+            {
+                "id": "2",
+                "url": "https://i.imgflip.com/4/1g8my4.jpg"
+            },
+            {
+                "id": "3",
+                "url": "https://i.imgflip.com/4/261o3j.jpg"
+            },
+            {
+                "id": "4",
+                "url": "https://i.imgflip.com/4/1tl71a.jpg"
+            },
+            {
+                "id": "5",
+                "url": "https://i.imgflip.com/4/5c7lwq.jpg"
+            },
+            {
+                "id": "6",
+                "url": "https://i.imgflip.com/4/2reqtg.jpg"
+            },
+            {
+                "id": "7",
+                "url": "https://i.imgflip.com/4/434i5j.jpg"
+            },
+            {
+                "id": "8",
+                "url": "https://i.imgflip.com/4/1op9wy.jpg"
+            },
+            {
+                "id": "9",
+                "url": "https://i.imgflip.com/4/265k.jpg"
+            },
+            {
+                "id": "10",
+                "url": "https://i.imgflip.com/4/39t1o.jpg"
+            }
+        ]
+    }
+}
